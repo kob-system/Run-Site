c0e05f076061b266a91a47f3e3c98888eabf8543
kob-system <your@email.com>
2026-05-13 22:40:45 -0400

    Hardcode Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://yvwpesvjfdofsxvtooha.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2d3Blc3ZqZmRvZnN4dnRvb2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MzI5NTEsImV4cCI6MjA5NDAwODk1MX0.aJFnTXuOqVmFVkMvuTdQls8gYkG6W9RXK-3kRKMK_RU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
